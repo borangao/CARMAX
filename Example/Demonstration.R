40862de27250543e6e9c055a14b9eff866488efb
ZikunY <yangzikun1125@gmail.com>
2024-10-16 10:34:36 -0400

    Code

---
+++ Example/Demonstration.R
@@ -0,0 +1,38 @@
+rm(list=ls())
+
+devtools::install_github("ZikunY/CARMAX")  
+
+library(CARMAX)
+setwd('/Volumes/My_Passport/1000Genome/Multi_study/test')# Set your local directory
+dt<-readRDS('./CARMAX/Example/Example.rds') 
+
+lambda.power<-1
+z.list<-list()
+ld.list<-list()
+full.w.list<-list()
+lambda.list<-c()
+label.list<-list()
+input.prior.list<-list()
+
+summ<-dt[[1]]
+p<-nrow(summ)
+
+
+
+g=1
+z.list[[g]]<-as.matrix(summ$BETA_EUR/summ$SE_EUR)
+lambda.list[[g]]<-lambda.power
+
+g=2
+z.list[[g]]<-as.matrix(summ$BETA_AMR/summ$SE_AMR)
+lambda.list[[g]]<-lambda.power
+
+ld.list[[1]]<-dt[[2]]
+ld.list[[2]]<-dt[[3]]
+prior.name='Spike-slab'
+results<-CARMAX(z.list,ld.list,lambda.list=lambda.list,outlier.switch=T,
+          effect.size.prior=prior.name,
+          epsilon.threshold=1e-3)
+
+
+
