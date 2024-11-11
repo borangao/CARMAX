e4d10f0f6a60c54f35552f65324cb3bd465b33b2
ZikunY <yangzikun1125@gmail.com>
2024-11-11 14:30:35 -0500

    Updating the example

--- Example/Demonstration.R
+++ Example/Demonstration.R
@@ -5,36 +5,33 @@ devtools::install_github("ZikunY/CARMAX")
 library(CARMAX)
 setwd('...')# Set your local directory
 dt<-readRDS('./CARMAX/Example/Example.rds') 
-
-summ<-read.table(paste0(''))
-
-lambda.power<-1
-z.list<-list()
-ld.list<-list()
-full.w.list<-list()
-lambda.list<-c()
-label.list<-list()
-input.prior.list<-list()
-
-summ<-dt[[1]]
-p<-nrow(summ)
-
+dt<-readRDS('/Volumes/My_Passport/1000Genome/Multi_study/data/summ_data/package/Example.rds')
+summ <- dt$`Summ data`
 head(summ)
+# Initialize lists
+lambda.power <- 1
+z.list <- list()
+ld.list <- list()
+full.w.list <- list()
+lambda.list <- c()
+label.list <- list()
+input.prior.list <- list()
+# Read the Z-scores and compute the LD for each ancestry 
+# AFR ancestry
+g <- 1
+z.list[[g]] <- as.matrix(summ$`Z_AFR`)
+lambda.list[[g]] <- lambda.power
+ld.list[[g]] <- cor(dt$`AFR genotype`)
+# EUR ancestry
+g <- 2
+z.list[[g]] <- as.matrix(summ$`Z_EUR`)
+lambda.list[[g]] <- lambda.power
+ld.list[[g]] <- cor(dt$`EUR genotype`)
 
-g=1
-z.list[[g]]<-as.matrix(summ$BETA_EUR/summ$SE_EUR)
-lambda.list[[g]]<-lambda.power
-
-g=2
-z.list[[g]]<-as.matrix(summ$BETA_AMR/summ$SE_AMR)
-lambda.list[[g]]<-lambda.power
-
-ld.list[[1]]<-dt[[2]] #######
-ld.list[[2]]<-dt[[3]]
 prior.name='Spike-slab'
-results<-CARMAX(z.list,ld.list,lambda.list=lambda.list,outlier.switch=T,
-          effect.size.prior=prior.name,
-          epsilon.threshold=1e-3)
 
+set.seed(124)
+
+results <- CARMAX(z.list, ld.list, lambda.list = lambda.list, effect.size.prior = prior.name, outlier.switch = TRUE)
 
 
